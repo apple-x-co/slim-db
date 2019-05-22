0ec8aebe2d52d4b346548a4c1f764f75d4aa5ca8
Kouhei Sano <sano@buddying.jp>
2019-05-22 18:14:53 +0900

    commit

--- htdocs/src/Model/User.php
+++ htdocs/src/Model/User.php
@@ -10,11 +10,21 @@ use Illuminate\Database\Eloquent\Model;
  * Class User
  * @package App\Model
  *
+ * @property int $id
  * @property string $name
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
  */
 class User extends Model
 {
     protected $table = 'users';
+
+    protected $guarded = ['id'];
+
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
--- htdocs/src/Model/UserLog.php
+++ htdocs/src/Model/UserLog.php
@@ -10,11 +10,22 @@ use Illuminate\Database\Eloquent\Model;
  * Class UserLog
  * @package App\Model
  *
- * @property string $name
+ * @property int $id
+ * @property int $user_id
+ * @property string $text
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
  */
 class UserLog extends Model
 {
     protected $table = 'user_logs';
+
+    protected $guarded = ['id'];
+
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
 
     public function user() {
         return $this->belongsTo(User::class);
